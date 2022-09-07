220e3d84d654d74f455aae3a6cfd64aec04f2b11
Klautts <47825278+Klautts@users.noreply.github.com>
2022-09-07 22:00:05 +0300

    css and html 50%

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,5 +1,37 @@
 import Item from "./Item";
 
+const itemInfo = [
+            {
+                imageSrc: "img/sneakers1.jpg",
+                description: "Мужские Кроссовки Nike Blazer Mid Suede",
+                price: "12 999"
+            },
+            {
+                imageSrc: "img/sneakers2.jpg",
+                description: "Мужские Кроссовки Nike Air Max 270",
+                price: "12 999"
+            },
+            {
+                imageSrc: "img/sneakers3.jpg",
+                description: "Мужские Кроссовки Nike Blazer Mid Suede",
+                price: "8 499"
+            },
+            {
+                imageSrc: "img/sneakers4.jpg",
+                description: "Кроссовки Puma X Aka Boku Future Rider",
+                price: "8 999"
+            },
+            {
+                imageSrc: "img/sneakers5.jpg",
+                description: "Мужские Кроссовки Under Armour Curry 8",
+                price: "15 199"
+            },
+            {
+                imageSrc: "img/sneakers6.jpg",
+                description: "Мужские Кроссовки Nike Kyrie 7",
+                price: "11 299"
+            },
+        ];
 function Content() {
   return (
     <div className="Content">
@@ -11,10 +43,7 @@ function Content() {
         </div>
       </div>
       <div className="WrapperCard">
-        <Item />
-        <Item />
-        <Item />
-        <Item />
+        {itemInfo.map((obj) => (<Item imageSrc={obj.imageSrc} description={obj.description} price={obj.price} obj={obj}/>))}
       </div>
     </div>
   );
